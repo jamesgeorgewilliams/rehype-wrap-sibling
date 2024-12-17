979daeb355a841572e14a64c1076903c132570e2
jamesgeorgewilliams <jamesgeorgewilliams@protonmail.com>
2024-12-17 10:27:41 +0100

    tests: add tests for plugin options

--- test/index.test.ts
+++ test/index.test.ts
@@ -17,36 +17,64 @@ const transformHTML = (
 		.processSync(html);
 };
 
-test('first test', async () => {
-	const inputHTML =
-		'<div><h1>Solar System</h1><h2>Formation and evolution</h2></div>';
-	const expectedHTML =
-		'<div><div class="container"><h1>Solar System</h1><h2>Formation and evolution</h2></div></div>';
+test('throw an error if the user provided selector is not a string', async () => {
+	const inputHTML = '<h1>Lorem</h1>';
 
-	const result = transformHTML(
-		rehypeNextSiblingWrap,
-		{
-			selector: 'h1',
-			wrapper: 'div.container',
-		},
-		inputHTML,
-	);
-	expect(result.value).toBe(expectedHTML);
+	expect(() =>
+		transformHTML(
+			rehypeNextSiblingWrap,
+			{
+				//@ts-ignore
+				selector: 1,
+				wrapper: 'div.container',
+			},
+			inputHTML,
+		),
+	).toThrowError('Expected a `string` as selector');
 });
 
-test('assert.equal', async () => {
-	const inputHTML =
-		'<div><h1>Solar System</h1><h2>Formation and evolution</h2></div>';
-	const expectedHTML =
-		'<div><div class="container"><h1>Solar System</h1><h2>Formation and evolution</h2></div></div>';
+test('throw an error if the user provided wrapper is not a string', async () => {
+	const inputHTML = '<h1>Lorem</h1>';
+
+	expect(() =>
+		transformHTML(
+			rehypeNextSiblingWrap,
+			{
+				selector: 'h1',
+				//@ts-ignore
+				wrapper: 1,
+			},
+			inputHTML,
+		),
+	).toThrowError('Expected a `string` as wrapper');
+});
+
+test("throw an error if user doesn't provide a selector option", async () => {
+	const inputHTML = '<h1>Lorem</h1>';
+
+	expect(() =>
+		transformHTML(
+			rehypeNextSiblingWrap,
+			//@ts-ignore
+			{
+				wrapper: 'div.container',
+			},
+			inputHTML,
+		),
+	).toThrowError('Expected a `string` as selector');
+});
+
+test("the default wrapper option is a div if the user doesn't provide one", async () => {
+	const inputHTML = '<h1>Lorem</h1><h2>Ipsum</h2>';
+	const expectedHTML = '<div><h1>Lorem</h1><h2>Ipsum</h2></div>';
 
 	const result = transformHTML(
 		rehypeNextSiblingWrap,
 		{
 			selector: 'h1',
-			wrapper: 'div.container',
 		},
 		inputHTML,
 	);
-	assert.equal(result.value, expectedHTML);
+
+	expect(result.value).toBe(expectedHTML);
 });
