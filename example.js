c1bbb7aa06554801a1616ea1178f06b154eefd19
jamesgeorgewilliams <jamesgeorgewilliams@protonmail.com>
2024-12-18 15:32:43 +0100

    chore: add static files dir

--- example.js
+++ example.js
@@ -2,11 +2,11 @@ import * as fs from 'node:fs/promises';
 import { rehype } from 'rehype';
 import rehypeNextSiblingWrap from './plugin.js';
 
-const document = await fs.readFile('input.html', 'utf8');
+const document = await fs.readFile('./static/input.html', 'utf8');
 
 const file = await rehype()
 	.data('settings', { fragment: true })
 	.use(rehypeNextSiblingWrap, { selector: 'h1', wrapper: 'div.container' })
 	.process(document);
 
-await fs.writeFile('output.html', String(file));
+await fs.writeFile('./static/output.html', String(file));
